2d5cfc064cef0001cc2e53fe14e3ac3dea6726f6
Nisal Nimsara <KUDSE232F-048@student.nibm.lk>
2025-09-18 08:44:31 +0530

    Updates layout text styling and content
    Refines the layout component by updating text styling to include padding, larger font size, centered alignment, and a bold font. Replaces placeholder text with more descriptive content to improve clarity and user experience.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const _layout = () => {
   return (
     <View>
-      <Text className='font-'>_layout</Text>
+      <Text className='pt-20 text-2xl text-center font-poppinsBold'>This one come from the{'\n'}(tabs) _layout</Text>
     </View>
   )
 }
