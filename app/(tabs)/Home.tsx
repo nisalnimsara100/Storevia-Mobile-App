5ae14c963e12717a2939c530061a6e912b289315
Janaka Namal Thennakoon <KUHDSE242F-005@student.nibm.lk>
2026-04-29 08:58:54 +0530

    Update Home.tsx

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -26,7 +26,7 @@ import FlashSaleCard from '../components/FlashSaleCard';
 import LargeProductTile from '../components/LargeProductTile';
 
 const BASE_URL = process.env.EXPO_PUBLIC_APP_BASE_URL;
-// test12
+// test123
 
 const { width } = Dimensions.get('window');
 
