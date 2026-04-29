2f05434657228d82f467b70fff2ec128c50f5064
Nisal Nimsara <nisalnimsara100@gmail.com>
2026-04-29 09:15:04 +0530

    Update Home.tsx

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -26,7 +26,6 @@ import FlashSaleCard from '../components/FlashSaleCard';
 import LargeProductTile from '../components/LargeProductTile';
 
 const BASE_URL = process.env.EXPO_PUBLIC_APP_BASE_URL;
-// test1
 
 const { width } = Dimensions.get('window');
 
