702c4b0bfe22f71ae430221f3df1ab412391b467
Nisal Nimsara <nisalnimsara100@gmail.com>
2026-04-29 09:00:50 +0530

    chore: remove test environment variable

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -26,7 +26,6 @@ import FlashSaleCard from '../components/FlashSaleCard';
 import LargeProductTile from '../components/LargeProductTile';
 
 const BASE_URL = process.env.EXPO_PUBLIC_APP_BASE_URL;
-const TEST_VAR = process.env.EXPO_PUBLIC_TEST_VAR;
 // test123
 
 const { width } = Dimensions.get('window');
